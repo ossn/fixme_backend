d8ca3a7e689a66b56ca103e4ba8213e3c7ee7341
Rajat Patwa <razat249@gmail.com>
2017-06-06 17:33:34 +0530

    feature to remove closed issues.

--- core/utils/services.py
+++ core/utils/services.py
@@ -8,7 +8,7 @@ def request_github_issues(user, repo):
     """
     Returns a list of all the issues of a repository in `json` format.
     """
-    api_data = 'https://api.github.com/repos/'+ user +'/' + repo + '/issues'
+    api_data = 'https://api.github.com/repos/'+ user +'/' + repo + '/issues?state=all'
     response = requests.get(api_data)
     if response.status_code < 400:
         return {'error': False, 'data': response.json()}
