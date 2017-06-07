9a3de1aa6a0479f6c16b651aa2d2bbacceed368d
Rajat Patwa <razat249@gmail.com>
2017-06-07 17:53:00 +0530

    configure mysql

--- issue_parser/settings.py
+++ issue_parser/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'issue_parser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'issue_parser',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
