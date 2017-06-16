43a78e82f17284ada4df636df1f6176fe18b949f
Rajat Patwa <razat249@gmail.com>
2017-06-16 14:06:12 +0530

    test the admin panel - UserRepo Model

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,33 @@
+"""
+This file contains all the tests related to `issue parser core` app.
+"""
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
 
-# Create your tests here.
+from .models import UserRepo
+
+class UserRepoModelTestCase(TestCase):
+    """This class defines the test suite for the `UserRepo` model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.user = 'razat249'
+        self.repo = 'github-view'
+        self.user_repo = UserRepo(user=self.user, repo=self.repo)
+
+    def test_user_repo_model_can_create_a_userrepo(self):
+        """Test the `UserRepo` model can create a `user_repo`."""
+        old_count = UserRepo.objects.count()
+        self.user_repo.save()
+        new_count = UserRepo.objects.count()
+        self.assertNotEqual(old_count, new_count)
+    
+    def test_user_repo_model_can_delete_a_userrepo(self):
+        """Test the `UserRepo` model can delete a `user_repo`."""
+        old_count = UserRepo.objects.count()
+        self.user_repo.save()
+        self.user_repo.delete()
+        new_count = UserRepo.objects.count()
+        self.assertEqual(old_count, new_count)
