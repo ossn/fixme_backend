b0fbeec963d2042c9755cae08b720fffaa467dd8
Rajat Patwa <razat249@gmail.com>
2017-06-19 21:08:56 +0530

    add error handling for no connection

--- core/utils/services.py
+++ core/utils/services.py
@@ -8,9 +8,15 @@ def request_github_issues(user, repo):
     """
     Returns a list of all the issues of a repository in `json` format.
     """
-    api_data = 'https://api.github.com/repos/'+ user +'/' + repo + '/issues?state=all'
-    response = requests.get(api_data)
-    if response.status_code < 400:
-        return {'error': False, 'data': response.json()}
-    else:
-        return {'error': True, 'data': response.json()}
+    try:
+        api_data = 'https://api.github.com/repos/'+ user +'/' + repo + '/issues?state=all'
+        response = requests.get(api_data)
+        if response.status_code < 400:
+            return {'error': False, 'data': response.json()}
+        else:
+            return {'error': True, 'data': response.json()}
+    except:
+        return {'error': True, 'data': 'There is no Internet connection'}
+
+
+print request_github_issues('razat249', 'github-view')
