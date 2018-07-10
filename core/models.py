3a5dd97eb9d540ebb5e00a5eefe9896dc46be542
Alexandros Dorodoulis <aldor94@gmail.com>
2018-07-10 19:37:31 +0300

    feat: Update model
    * Add projects and repos to issues
    * Stop worker from saving pull requests to the db
    * Get issue info from labels

--- core/models.py
+++ core/models.py
@@ -4,13 +4,35 @@ from __future__ import unicode_literals
 from datetime import timedelta
 from django.db import models
 from django_mysql.models import ListTextField
-from core.utils.services import request_github_issues
+from core.utils.services import request_to_github, get_repo_and_username
 from celery.decorators import periodic_task, task
 import logging
 
 ISSUE_UPDATE_PERIOD = 15  # in minutes
-
+"""
+ These labels are going count as "easy" for the experience needed
+"""
 EASY_LABELS = ["help_wanted", "good first issue", "easyfix", "easy"]
+
+"""
+ These labels are going count as "moderate" for the experience needed
+"""
+MODERATE_LABELS = ["moderate"]
+
+"""
+These labels are going count as "senior" for the experience needed
+"""
+SENIOR_LABELS = ["senior"]
+
+"""
+These labels are going count as "enhancement" for the issue type
+"""
+ENHANCEMENT_LABELS = ["enhancement"]
+
+"""
+These labels are going count as "bugfix" for the issue type
+"""
+BUGFIX_LABELS = ["bug", "bugfix"]
 
 
 class Project(models.Model):
@@ -28,7 +50,7 @@ class Project(models.Model):
     logo = models.URLField()
     link = models.URLField()
     setup_duration = models.CharField(max_length=100, blank=True)
-    tags = ListTextField(base_field=models.CharField(max_length=50), size=100)
+    tags = ListTextField(base_field=models.CharField(max_length=80), size=100)
     issues_count = models.IntegerField()
 
     class Meta:
@@ -86,22 +108,26 @@ class Issue(models.Model):
     )
     # Model attributes start from here.
     issue_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=100)
+    title = models.TextField()
     experience_needed = models.CharField(
-        max_length=10,
+        max_length=50,
         choices=EXPERIENCE_NEEDED_CHOICES,
         default=MODERATE,
     )
     expected_time = models.CharField(max_length=100)
-    language = models.CharField(max_length=100)
-    tech_stack = models.CharField(max_length=100)
+    language = models.TextField()
+    tech_stack = models.TextField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     issue_number = models.IntegerField()
     issue_labels = models.ManyToManyField(IssueLabel, blank=True)
     issue_url = models.URLField()
     issue_body = models.TextField()
-    issue_type = models.CharField(max_length=100, default="")
+    issue_type = models.TextField(default="")
+    repo = models.ForeignKey(
+        Repository, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(
+        Project, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         ordering = ('updated_at',)  # Ascending order according to updated_at.
@@ -118,23 +144,32 @@ def periodic_issues_updater():
     """
     list_of_repos = Repository.objects.values('repository_url',)
     for repo in list_of_repos:
-        issue_list = request_github_issues(
-            repo['repository_url'])
+        repository, username = get_repo_and_username(repo['repository_url'])
+        language_request = request_to_github(repository, username, "")
+        if language_request['error']:
+            logging.error("Error" + str(language_request['data']))
+            continue
+        language = language_request['data']['language']
+        issue_list = request_to_github(
+            repository, username, '/issues?state=all')
         if issue_list['error']:
             logging.error("Error" + str(issue_list['data']))
-        else:
-            for issue in issue_list['data']:
-                validate_and_store_issue(issue)
+            continue
+        for issue in issue_list['data']:
+            validate_and_store_issue(issue, language, repo)
 
 
-def validate_and_store_issue(issue):
+def validate_and_store_issue(issue, language, repo):
     """
     Validate issue:- if valid - store it into database,
     else - Do not store in database
     """
     if is_issue_state_open(issue):
-        if is_issue_valid(issue):
-            store_issue_in_db(issue)
+        repo_instace = Repository.objects.get(
+            repository_url=repo.get("repository_url"))
+        project = repo_instace.project
+        store_issue_in_db(
+            issue, language, project, repo_instace)
 
 
 def is_issue_state_open(issue):
@@ -142,6 +177,9 @@ def is_issue_state_open(issue):
     Returns true if issue state is open else
     return false and delete the issue from database.
     """
+    if issue.get('pull_request'):
+        return False
+
     if issue['state'] == 'open':
         return True
     else:
@@ -149,49 +187,40 @@ def is_issue_state_open(issue):
         return False
 
 
-def is_issue_valid(issue):
-    """
-    Checks if issue is valid for system or not.
-    Return True if valid else return false.
-    """
-    parsed = parse_issue(issue['body'])
-    for item in parsed:
-        if not item:
-            return False  # issue is not valid
-    logging.warning('Issue with id ' +
-                    str(issue['id']) + ' is not valid for our system.')
-    return True  # issue is valid
+# FIXME: Rewrite and cleanup this
 
 
-def store_issue_in_db(issue):
+def store_issue_in_db(issue, language, project, repo):
     """Stores issue in db"""
-    experience_needed, language, expected_time, technology_stack = parse_issue(
-        issue['body'])
-    experience_needed = experience_needed.strip().lower()
-    if experience_needed in EASY_LABELS:
-        experience_needed = "easy"
-    language = language.strip().lower()
-    expected_time = expected_time.strip().lower()
-    technology_stack = technology_stack.strip().lower()
-    issue_type = ""
-    issue_instance = Issue(issue_id=issue['id'], title=issue['title'],
-                           experience_needed=experience_needed, expected_time=expected_time,
-                           language=language, tech_stack=technology_stack,
-                           created_at=issue['created_at'], updated_at=issue['updated_at'],
-                           issue_number=issue['number'], issue_url=issue['html_url'],
-                           issue_body=issue['body'], issue_type=issue_type)
-    issue_instance.save()
-    for label in issue['labels']:
-        try:
-            if label['name'].lower() in ['enhancement', 'bugfix', 'task']:
-                issue_instance.issue_type = label['name'].lower()
-                issue_instance.save()
-        except:
-            logging.warning('Couldn\'t parse label: ' + label)
-        label_instance = IssueLabel(label_id=label['id'], label_name=label['name'],
-                                    label_url=label['url'], label_color=label['color'])
-        label_instance.save()
-        issue_instance.issue_labels.add(label_instance)
+    # experience_needed, bodyLanguage, expected_time, technology_stack = parse_issue(
+    #     issue['body'])
+    # experience_needed = parse_experience(experience_needed.strip().lower())
+    # expected_time = expected_time.strip().lower()
+    # technology_stack = technology_stack.strip().lower()
+    try:
+        language = language.strip().lower()
+        issue_type = ""
+        issue_instance = Issue(issue_id=issue['id'], title=issue['title'], language=language, created_at=issue['created_at'],
+                               updated_at=issue['updated_at'], issue_number=issue['number'], issue_url=issue['html_url'], issue_body=issue['body'], issue_type=issue_type, project=project, repo=repo)
+        issue_instance.save()
+        for label in issue['labels']:
+            try:
+                name = label['name'].lower()
+                if name in ENHANCEMENT_LABELS + BUGFIX_LABELS:
+                    issue_instance.issue_type = parse_type(name)
+                    issue_instance.save()
+                elif name in SENIOR_LABELS + EASY_LABELS + MODERATE_LABELS:
+                    issue_instance.experience_needed = parse_experience(name)
+                    issue_instance.save()
+
+            except Exception as e:
+                logging.error('Couldn\'t parse label: ' + label + e)
+            label_instance = IssueLabel(label_id=label['id'], label_name=label['name'],
+                                        label_url=label['url'], label_color=label['color'])
+            label_instance.save()
+            issue_instance.issue_labels.add(label_instance)
+    except Exception as e:
+        logging.error("Couldn't parse issue: " + issue + e)
 
 
 def delete_closed_issues(issue):
@@ -227,3 +256,21 @@ def find_between(string, first, last):
         return string[start:end].replace(': ', '')
     except ValueError:
         return ""
+
+
+def parse_experience(experience_needed):
+    if experience_needed in EASY_LABELS:
+        return "easy"
+    elif experience_needed in MODERATE_LABELS:
+        return "moderate"
+    elif experience_needed in SENIOR_LABELS:
+        return "senior"
+    return experience_needed
+
+
+def parse_type(issue_type):
+    if issue_type in BUGFIX_LABELS:
+        return "bugfix"
+    elif issue_type in ENHANCEMENT_LABELS:
+        return "enhancement"
+    return issue_type
